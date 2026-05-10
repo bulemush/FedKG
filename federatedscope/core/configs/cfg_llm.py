cc2df692787a62951dc34fda16efe84777c7f99a
unknown <dzkjwanggehui@outlook.com>
2026-05-10 16:22:41 +0800

    OOM_cuda_1

--- federatedscope/core/configs/cfg_llm.py
+++ federatedscope/core/configs/cfg_llm.py
@@ -53,7 +53,7 @@ def extend_llm_cfg(cfg):
     # ---------------------------------------------------------------------- #
     cfg.llm.model_parallel = CN(new_allowed=True)
     cfg.llm.model_parallel.use = False
-    cfg.llm.model_parallel.device_map = 'auto'
+    cfg.llm.model_parallel.device_map = 'balanced_layers'
     cfg.llm.model_parallel.same_device_map = False
     cfg.llm.model_parallel.coexisting_model_ratio = 0.45
 
